8717a83e31d6da8e9e12e360e43d42a09b367df1
devangg37 <devangg37@example.com>
2022-06-27 19:35:27 +0900

    Dropdown Added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
+import DropDown from './components/DropDown.vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+const options : Array<String> = ['1x', '2x'];
+const font : Object = {width: "w-[400px]" ,  height: "h-[70px]"}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <DropDown :options = options :font=font />
 </template>
 
 <style>
